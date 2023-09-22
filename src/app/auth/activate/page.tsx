6a32340b8fd693ab35763a4ec3df74a66ffb4cf0
chrisezedi <91887106+chrisezedi@users.noreply.github.com>
2023-09-22 23:56:32 +0100

    finalise account activation

--- src/app/auth/activate/page.tsx
+++ src/app/auth/activate/page.tsx
@@ -1,36 +1,46 @@
 'use client'
 
-import axios, {AxiosError} from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import ErrorCard from '@/components/errors/errors';
 import { Error } from '@/types';
 import { useToast } from '@/components/ui/toast/use-toast';
+import { Card, CardContent, CardHeader } from '@/components/ui/card/card';
+import Image from 'next/image';
 
 const ActivateAccount = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
     const uid = searchParams.get('uid');
     const token = searchParams.get('token');
-    const [error, setError] = useState<Error>();
+    const [error, setError] = useState<Error | null>();
     const [loading, setLoading] = useState<boolean>();
+    const [mailSent, setMailSent] = useState<string>('');
+
     const { toast } = useToast();
 
-    const resendActivationMail = async() => {
-       try {
-        setLoading(true);
-        const response = await axios.post('http://127.0.0.1:8000/auth/resendactivationmail',{ uid })
+    const setErrorResponse = (error: unknown) => {
+        const axiosError = error as AxiosError;
+        const errorMsg = axiosError.response?.data as Error
+        setError(errorMsg);
+    }
 
-        console.log(response);
-        if (response.data?.redirect) {
-            toast({title:'Activated Account',description:response.data.message});
-            router.push(response.data?.redirect)
-        }else{
-            toast({title:'Activation Mail Sent',description:response.data.message});
+    const resendActivationMail = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post('http://127.0.0.1:8000/auth/resendactivationmail', { uid, token })
+
+            if (response.data?.redirect) {
+                toast({ title: 'Activated Account', description: response.data.message });
+                router.push(response.data?.redirect)
+            } else {
+                setError(null);
+                setMailSent(response.data?.message);
+            }
+        } catch (error) {
+            setErrorResponse(error)
         }
-       } catch (error) {
-        console.log(error)
-       }
 
         setLoading(false);
     }
@@ -38,23 +48,33 @@ const ActivateAccount = () => {
     useEffect(() => {
         const activateAccount = async () => {
             try {
-                const response = await axios.put('http://127.0.0.1:8000/auth/activate',{"uid":uid})
-
-                console.log(response)
-                // response.ok ? router.push('/') : setError(errorMsg);
+                const response = await axios.put('http://127.0.0.1:8000/auth/activate', { uid, token });
+                if (response) {
+                    toast({ title: 'Account Activated', description: response.data.message });
+                    router.push('/');
+                }
             } catch (error) {
-                const axiosError = error as AxiosError;
-                const errorMsg = axiosError.response?.data as Error
-                setError(errorMsg);
+                setErrorResponse(error);
             }
         }
         activateAccount();
     }, [])
 
     return (
-    <div>
-        {error && <ErrorCard error={error} resendActivationMail={resendActivationMail} loading={loading}/>}
-    </div>
+        <div className="flex justify-center w-full h-screen items-center">
+            {error && <ErrorCard error={error} resendActivationMail={resendActivationMail} loading={loading} />}
+            {
+                (mailSent && !error) &&
+                <Card className='max-w-[90%] md:min-w-[30%]'>
+                    <CardHeader><h1 className='text-center'>Activation Mail Sent</h1></CardHeader>
+                    <CardContent>
+                        <Image className="mx-auto" src="/mailsent.svg" alt="mailsent image" width="200" height="200" />
+                        <p className='text-center mt-8'>{mailSent}</p>
+                    </CardContent>
+                </Card>
+
+            }
+        </div>
     )
 }
 
